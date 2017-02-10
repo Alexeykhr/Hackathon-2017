f98fb87b8b35ebf31eb0e44c66a5383a8b07cebe
Alexey Khrusch <alexeykhr@outlook.com>
2017-02-10 12:58:55 +0200

    release 1.0

--- cron.php
+++ cron.php
@@ -3,9 +3,9 @@
 require_once __DIR__ . '/config.php';
 
 $screen = new ScreenShots();
-$screen->saveImageFromWeb( $screen->makeScreenshots('') );
+$screen->saveImageFromWeb( $screen->makeScreenshots() ); // Головна сторінка
 
-$vk = new VK('', 139842925, '5.62');
+$vk = new VK('', 0, '5.62'); // Token User, GroupID, Version API
 $photo = $vk->uploadImageOnServer('screen.png');
 $res = $vk->photosSaveWallPhoto($photo->server, $photo->photo, $photo->hash);
 $vk->wallPost('', 'photo' . $res->response[0]->owner_id . '_' . $res->response[0]->id);
